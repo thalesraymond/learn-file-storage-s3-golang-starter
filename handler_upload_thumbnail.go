26ab173b2e5a64155187ec76dcd8676ec59278c0
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2026-08-07 19:47:13 -0300

    remove thumbnail handling and implement thumbnail upload functionality

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +29,78 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	r.ParseMultipartForm(10 << 20) // 10 MB
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, _, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't read thumbnail file", err)
+		return
+	}
+	defer file.Close()
+
+	// Read the file into a byte slice
+	fileBytes := make([]byte, 0)
+	buffer := make([]byte, 1024)
+	for {
+		n, err := file.Read(buffer)
+		if err != nil {
+			break
+		}
+		fileBytes = append(fileBytes, buffer[:n]...)
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video from database", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You don't have permission to upload a thumbnail for this video", nil)
+		return
+	}
+
+	fileExtension := ""
+	contentType := http.DetectContentType(fileBytes)
+	switch contentType {
+	case "image/jpeg":
+		fileExtension = ".jpg"
+	case "image/png":
+		fileExtension = ".png"
+	case "image/gif":
+		fileExtension = ".gif"
+	default:
+		respondWithError(w, http.StatusBadRequest, "Unsupported file type", fmt.Errorf("unsupported file type: %s", contentType))
+		return
+	}
+
+	filePath := fmt.Sprintf("%s/%s%s", cfg.assetsRoot, videoID.String(), fileExtension)
+
+	// Save locally simulating S3 upload
+	out, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create thumbnail file", err)
+		return
+	}
+	defer out.Close()
+
+	_, err = out.Write(fileBytes)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't write thumbnail file", err)
+		return
+	}
+
+	// main go has 	mux.Handle("/assets/", cacheMiddleware(assetsHandler))
+	thumbnailURL := fmt.Sprintf("/assets/%s%s", videoID.String(), fileExtension)
+	video.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video in database", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
