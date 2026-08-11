b213964e438936d54bb34ea25c93f69a7aaeda91
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2026-08-10 22:30:22 -0300

    feat: implement video upload handler with JWT validation and file type checking

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -2,6 +2,77 @@ package main
 
 import (
 	"net/http"
+	"os"
+
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	http.MaxBytesReader(w, r.Body, 1<<30)
+
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video from database", err)
+		return
+	}
+
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You don't have permission to upload a thumbnail for this video", nil)
+		return
+	}
+
+	r.ParseMultipartForm(10 << 30) // 1GB
+	file, _, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get video file", err)
+		return
+	}
+
+	defer file.Close()
+
+	fileBytes := make([]byte, 0)
+	buffer := make([]byte, 1024)
+	for {
+		n, err := file.Read(buffer)
+		if err != nil {
+			break
+		}
+		fileBytes = append(fileBytes, buffer[:n]...)
+	}
+
+	fileExtension := ""
+	contentType := http.DetectContentType(fileBytes)
+	switch contentType {
+	case "video/mp4":
+		fileExtension = ".mp4"
+	default:
+		respondWithError(w, http.StatusBadRequest, "Invalid file type", nil)
+		return
+	}
+
+	tempFileName := "upload-" + videoID.String() + "-video" + fileExtension
+	tempFile, err := os.CreateTemp("", tempFileName)
+
+	defer os.Remove(tempFile.Name())
+}
